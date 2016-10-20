abfefadf1dfe8604c33532830574333278dc23c9
Remco Weekers <remco@craftsmen.nl>
2016-10-20 16:07:17 +0200

    Add exercise 3

---
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/excercises/Excercise3.java
@@ -0,0 +1,14 @@
+package nl.craftsmen.workshops.reactivemeetup.excercises;
+
+import nl.craftsmen.workshops.reactivemeetup.util.ExampleStreams;
+import rx.Observable;
+
+public class Excercise3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Observable<Integer> number$ = ExampleStreams.number$();
+		number$.filter((i) -> i % 2 == 0).subscribe(System.out::println);
+		
+		Thread.sleep(5000);
+	}
+}
