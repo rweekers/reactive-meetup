bb1818bd0dd80b65a7216464f9999f7b6093a487
Daan Scheerens <d.scheerens@gmail.com>
2016-11-03 11:14:22 +0100

    Added support for rounding numbers at two decimals in excercise 9.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/excercises/Excercise09.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/excercises/Excercise09.java
@@ -2,15 +2,18 @@ package nl.craftsmen.workshops.reactivemeetup.excercises;
 
 import nl.craftsmen.workshops.reactivemeetup.domain.IShape;
 import nl.craftsmen.workshops.reactivemeetup.util.ExampleStreams;
+import nl.craftsmen.workshops.reactivemeetup.util.Utils;
 import rx.Observable;
 
 public class Excercise09 {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		Observable<IShape> shape$ = ExampleStreams.shape$();
 		shape$
 				.filter((i) -> i.calculateArea() > 500)
                 .map(IShape::calculateCircumference)
+                .map(Utils.NUMBER_FORMAT::format)
 				.subscribe(System.out::println);
 
 		Thread.sleep(6000);
