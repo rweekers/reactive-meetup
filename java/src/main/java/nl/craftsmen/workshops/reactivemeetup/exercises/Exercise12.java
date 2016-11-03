ba910716014fd5893a7cc313966c48790a18100e
Remco Weekers <remco@craftsmen.nl>
2016-11-03 11:53:16 +0100

    Add exercise 12

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/Exercise12.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/Exercise12.java
@@ -3,13 +3,17 @@ package nl.craftsmen.workshops.reactivemeetup.exercises;
 import nl.craftsmen.workshops.reactivemeetup.util.ExampleStreams;
 import rx.Observable;
 
+import static nl.craftsmen.workshops.reactivemeetup.util.Utils.waitForStreamToComplete;
+
 public class Exercise12 {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		Observable<Integer> point$ = ExampleStreams.point$();
 		point$
+                .scan((x, y) -> x + y)
+                .zipWith(point$, (total, points) -> "Points: " + points + " - total: " + total)
 				.subscribe(System.out::println);
 
-		Thread.sleep(6000);
+        waitForStreamToComplete(point$);
 	}
 }
