33ee2b29bbb2558fc0fd91b4aa240459f028d34c
Remco Weekers <remco@craftsmen.nl>
2016-11-03 11:28:57 +0100

    Format average

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/Exercise10.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/Exercise10.java
@@ -3,6 +3,7 @@ package nl.craftsmen.workshops.reactivemeetup.exercises;
 import nl.craftsmen.workshops.reactivemeetup.domain.IShape;
 import nl.craftsmen.workshops.reactivemeetup.domain.Subtotal;
 import nl.craftsmen.workshops.reactivemeetup.util.ExampleStreams;
+import nl.craftsmen.workshops.reactivemeetup.util.Utils;
 import rx.Observable;
 
 public class Exercise10 {
@@ -14,6 +15,7 @@ public class Exercise10 {
                 .scan(new Subtotal(0, 0), (acc, shape) -> acc.add(shape.calculateArea()))
                 .skip(1)
                 .map((subtotal) -> subtotal.getSum() / subtotal.getCount())
+                .map(Utils.NUMBER_FORMAT::format)
                 .subscribe(System.out::println);
 
         Thread.sleep(6000);
