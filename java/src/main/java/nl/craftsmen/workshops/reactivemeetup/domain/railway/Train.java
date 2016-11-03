b42e0c62dbe48bd867f1330173d5b1f80896a138
Remco Weekers <remco@craftsmen.nl>
2016-11-03 16:11:18 +0100

    Add train domain class

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/domain/railway/Train.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/domain/railway/Train.java
@@ -1,11 +1,11 @@
-package nl.craftsmen.workshops.reactivemeetup.util;
+package nl.craftsmen.workshops.reactivemeetup.domain.railway;
 
 public class Train {
 
     private double longitude;
     private double latitude;
 
-    public(double latitude, double longitude) {
+    public Train(double latitude, double longitude) {
         this.longitude = longitude;
         this.latitude = latitude;
     }
