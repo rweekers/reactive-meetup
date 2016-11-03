331063e2ffe537837b03222b16519be0e8fcfa78
Daan Scheerens <d.scheerens@gmail.com>
2016-11-03 16:02:50 +0100

    Added assignment descriptions for exercises 8 through 12. Removed rxjava-math dependency.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise09.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise09.java
@@ -10,12 +10,21 @@ import rx.Observable;
 public class Exercise09 {
 
 	public static void main(String[] args) {
+		// The shape$ stream emits a sequence of varying shapes.
 		Observable<IShape> shape$ = ExampleStreams.shape$();
 		
-		shape$
+		// ASSIGNMENT: Display the circumference for all shapes whose surface area is larger than 500.
+		//
+		// HINT: You do not need to compute the circumference and surface area yourself. Instead you can use the following
+		// functions calculateCircumference() and calculateArea() provide by the IShape interface. 
+		Observable<Double> circumference$ = shape$
 			.filter((i) -> i.calculateArea() > 500)
-            .map(IShape::calculateCircumference)
-            .map(Utils.NUMBER_FORMAT::format)
+            .map(IShape::calculateCircumference);
+		
+		// If implemented correctly you should see the following values:
+		//   188.50, 251.33, 2000.00, 3600.00, 120.00, 145.21, 1400.00
+		
+		circumference$.map(Utils.NUMBER_FORMAT::format)
 			.subscribe(System.out::println);
 
 		waitForStreamToComplete(shape$);
