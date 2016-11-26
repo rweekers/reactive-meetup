866f10f50e5e352cc8a16c7497e8d4d6bf610094
Daan Scheerens <d.scheerens@gmail.com>
2016-11-26 12:25:31 +0100

    Added assignment description for exercise 2.6.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/two/Exercise06.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/two/Exercise06.java
@@ -15,8 +15,22 @@ public class Exercise06 {
 		
 		TravelCostMatrix costMatrix = RailwayStreams.travelCostMatrix();
 		
-		// ASSIGNMENT:
+		// Each check-in and check-out event is associated with an "OV chipkaart". The gateCheckEvent$ stream below represents the stream
+		// of check-in and check-out events for a single "OV chipkaart". For every check-in/out the railway station is recorded. This
+		// information can be used to compute the travel cost for a journey. The price for journey from railway station A to B can be
+		// determined through the costMatrix object above. It might be possible that someone forgets to check in or check out. This
+		// situation can be detected by two successive check-in events. In that case the travel cost is 20 euros.
 		Observable<GateCheckEvent> gateCheckEvent$ = RailwayStreams.personalCheckinsCheckouts$();
+		
+		// ASSIGNMENT: Given the gateCheckEvent$ of check-in and check-out events compute the cumulative travel cost. The resulting stream
+		// should emit the total travel cost for every new journey.
+		//
+		// HINT: To solve this exercise you will first need to find a method to obtain a stream of two successive gate check-in/out events.
+		//
+		// HINT: For each pair (a, b) of GateCheckEvents use the following rules to determine the travel cost:
+		//  - a.isCheckIn() && b.isCheckOut()  ->  costMatrix.getTravelCost(a.getRailwayStation(), b.getRailwayStation())
+		//  - a.isCheckIn() && b.isCheckIn()   ->  NO_CHECKOUT_COST
+		//  - otherwise                        ->  Nothing. You can represent this using one of the following: Optional.empty(), null or 0.0
 		
 		Observable<Double> travelCost$ = Observable
 			.zip(gateCheckEvent$, gateCheckEvent$.skip(1), (a, b) -> {
@@ -29,6 +43,9 @@ public class Exercise06 {
 			})
 			.filter((cost) -> cost > 0)
 			.scan((a, b) -> a + b);
+		
+		// When implemented correctly you should see the following output:
+		// 7.5, 19.0, 39.0, 46.5
 		
 		travelCost$.subscribe(System.out::println);
 		
