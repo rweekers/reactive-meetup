29d4a1f9dbb0e8c17315291a050aa4dfbd1fefab
Daan Scheerens <d.scheerens@gmail.com>
2016-11-29 11:08:31 +0100

    Implemented reactive solution for the train journey simulator.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/domain/railway/TrainJourneySimulation.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/domain/railway/TrainJourneySimulation.java
@@ -1,119 +1,62 @@
 package nl.craftsmen.workshops.reactivemeetup.domain.railway;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
-import rx.subjects.ReplaySubject;
-import rx.subjects.Subject;
 
 public class TrainJourneySimulation {
 	
-	private final TrainJourneySimulationParameters simulationParameters;
+	private final TrainJourneySimulationParameters parameters;
 	
 	public TrainJourneySimulation(TrainJourneySimulationParameters simulationParameters) {
-		this.simulationParameters = simulationParameters;
+		this.parameters = simulationParameters;
 	}
 	
 	public Observable<TrainMetrics> trainMetrics$() {
 		
-		Subject<TrainMetrics, TrainMetrics> publisher = ReplaySubject.create();
+		LatLong startPosition = parameters.getStart().getLocation();
+		LatLong destinationPosition = parameters.getDestination().getLocation();
+		double totalDistance = startPosition.distanceTo(destinationPosition);
 		
-		SimulationRunner simulationRunner = new SimulationRunner(simulationParameters, publisher);
+		double accelerationTime = parameters.getMaxVelocity() / parameters.getAcceleration();
+		double accelerationDistance = 0.5 * parameters.getAcceleration() * accelerationTime * accelerationTime;
 		
-		Thread simulationThread = new Thread(simulationRunner);
+		// TODO adjust acceleration time and distance when unable to achieve max velocity for short distances.
 		
-		simulationThread.setDaemon(true);
-		simulationThread.start();
+		double unacceleratedDistance = totalDistance - 2 * accelerationDistance;
+		double unacceleratedTime = unacceleratedDistance / parameters.getMaxVelocity();
 		
-		return publisher.asObservable();
-	}
-	
-	private static class SimulationRunner implements Runnable {
+		double totalTime = 2 * accelerationTime + unacceleratedTime;
 		
-		private final TrainJourneySimulationParameters parameters;
+		long startTime = System.currentTimeMillis();
 		
-		private final Subject<TrainMetrics, TrainMetrics> publisher;
+		double tickDelay = 1000.0 / parameters.getTickFrequency();
 		
-		public SimulationRunner(TrainJourneySimulationParameters simulationParameters, Subject<TrainMetrics, TrainMetrics> publisher) {
-			this.parameters = simulationParameters;
-			this.publisher = publisher;
-		}
-
-		@Override
-		public void run() {
-			
-			LatLong startPosition = parameters.getStart().getLocation();
-			LatLong destinationPosition = parameters.getDestination().getLocation();
-			double totalDistance = startPosition.distanceTo(destinationPosition);
-			
-			double accelerationTime = parameters.getMaxVelocity() / parameters.getAcceleration();
-			double accelerationDistance = 0.5 * parameters.getAcceleration() * accelerationTime * accelerationTime;
-			
-			// TODO adjust acceleration time and distance when unable to achieve max velocity for short distances.
-			
-			double unacceleratedDistance = totalDistance - 2 * accelerationDistance;
-			double unacceleratedTime = unacceleratedDistance / parameters.getMaxVelocity();
-			
-			double totalTime = 2 * accelerationTime + unacceleratedTime;
-			
-			long startTime = System.currentTimeMillis();
-			
-			try {
+		int requiredNumberOfFrames = (int) Math.ceil(totalTime * 1000 / (tickDelay * parameters.getTimeDilation()));
+		
+		return Observable.interval((long) Math.round(tickDelay), TimeUnit.MILLISECONDS)
+			.take(requiredNumberOfFrames)
+			.map((frameIndex) -> {
 				
-				loop(parameters.getTickFrequency(), () -> {
-					
-					double elapsedTime = (System.currentTimeMillis() - startTime) / 1000.0 * parameters.getTimeDilation();
-					
-					double distance;
-					if (elapsedTime < accelerationTime) {
-						distance = 0.5 * parameters.getAcceleration() * elapsedTime * elapsedTime;
-					} else if (elapsedTime < unacceleratedTime + accelerationTime) {
-						distance = accelerationDistance + parameters.getMaxVelocity() * (elapsedTime - accelerationTime);
-					} else {
-						double t = Math.min(accelerationTime, elapsedTime - accelerationTime - unacceleratedTime);
-						distance = Math.min(totalDistance, accelerationDistance + unacceleratedDistance +
-							parameters.getMaxVelocity() * t - 0.5 * parameters.getAcceleration() * t * t);
-					}
-					
-					double normalizedDistance = distance / totalDistance;
-					
-					LatLong currentPosition = startPosition.interpolate(destinationPosition, normalizedDistance);
-					
-					publisher.onNext(new TrainMetrics(parameters.getTrainId(), startTime + (long)(elapsedTime * 1000), currentPosition));
-					
-					return elapsedTime < totalTime;
-				});
+				double elapsedTime = (frameIndex * tickDelay) / 1000.0 * parameters.getTimeDilation();
 				
-				publisher.onCompleted();
-				
-			} catch (Exception e) {
-				
-				publisher.onError(e);
-				
-			}
-			
-		}
-		
-		private static void loop(int frequency, Callable<Boolean> action) throws Exception {
-			double tickDelay = 1000.0 / frequency;
-			
-			boolean shouldContinue = true;
-			try {
-				long startTime = System.currentTimeMillis() - (long)(tickDelay + 0.5);
-				while (shouldContinue) {
-					long elapsedTime = System.currentTimeMillis() - startTime;
-					long sleepTime = Math.max(0, (long)(2 * tickDelay - elapsedTime + 0.5));
-					
-					startTime = System.currentTimeMillis();
-					
-					Thread.sleep(sleepTime);
-					
-					shouldContinue = action.call();
+				double distance;
+				if (elapsedTime < accelerationTime) {
+					distance = 0.5 * parameters.getAcceleration() * elapsedTime * elapsedTime;
+				} else if (elapsedTime < unacceleratedTime + accelerationTime) {
+					distance = accelerationDistance + parameters.getMaxVelocity() * (elapsedTime - accelerationTime);
+				} else {
+					double t = Math.min(accelerationTime, elapsedTime - accelerationTime - unacceleratedTime);
+					distance = Math.min(totalDistance, accelerationDistance + unacceleratedDistance +
+						parameters.getMaxVelocity() * t - 0.5 * parameters.getAcceleration() * t * t);
 				}
-			} catch (InterruptedException e) {
-				// Interrupted. Silently quit loop.
-			}
-		}
+				
+				double normalizedDistance = distance / totalDistance;
+				
+				LatLong currentPosition = startPosition.interpolate(destinationPosition, normalizedDistance);
+				
+				return new TrainMetrics(parameters.getTrainId(), startTime + (long)(elapsedTime * 1000), currentPosition);
+			});
 	}
 	
 }
