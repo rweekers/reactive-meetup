d39f5b2ed8e634d57f0d17113b01bd273d72b710
Daan Scheerens <d.scheerens@gmail.com>
2016-12-02 09:11:47 +0100

    Added assignment description for Java exercise 2.7.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/two/Exercise07.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/two/Exercise07.java
@@ -11,11 +11,31 @@ public class Exercise07 {
 
 	public static void main(String[] args) {
 		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		Observable<TrainMetrics> trainMetrics$ = RailwayStreams.trainMetrics$();
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// ASSIGNMENT: Compute the average velocity (in Km/h) of the train using the data from the trainMetrics$ stream. The average
+		// velocity should be updated in real time so your output should again be a stream, only this time it should emit average velocity
+		// values. Compute the "real time" velocity every time over a set of 10 subsequent TrainMetrics values emitted from the
+		// trainMetrics$ stream. Do this once out of every 5 times a value gets emitted from the trainMetrics$ stream. Finally, determine
+		// the maximum velocity of train using the output from the averageVelocity$ stream.
+		//
+		// HINT: You can compute the average velocity for a set of subsequent TrainMetrics values by using only the first and last value
+		// from that set. The velocity is then computed by taking the difference between the position of the first and last element
+		// divided by the time difference between those to values.
+		//
+		// HINT: The distance (in meters) between two TrainMetrics values x and y can be computed as follows:
+		//  - x.getPosition().distanceTo(y.getPosition())
+		//
+		// HINT: Time difference (in seconds) between two TrainMetrics values x and y is computed in the following way:
+		//  - Math.abs(x.getTimestamp() - y.getTimestamp()) / 1000.0
+		//
+		// HINT: To convert a velocity in meters per second (m/s) to kilometers per hour (Km/h) multiple the velocity with 3.6
 		
 		Observable<Double> averageVelocity$ = trainMetrics$
 			.buffer(10, 5)
-			.filter((measurements) -> measurements.size() > 1)
 			.map((measurements) -> {
 				TrainMetrics first = measurements.get(0);
 				TrainMetrics last = measurements.get(measurements.size() - 1);
@@ -26,6 +46,7 @@ public class Exercise07 {
 			})
 			.map((velocity) -> velocity * 3.6);
 		
+		// When implemented correctly you should find a maximum velocity of ~ 140 Km/h.
 		averageVelocity$.subscribe(System.out::println);
 		
 		waitForStreamToComplete(averageVelocity$);
